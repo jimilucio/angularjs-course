b1d0733c8e45fb4c33d7c72386f004b59b532a0e
Luciano Murruni <luciano.murruni@conte.it>
2018-04-19 18:48:29 +0200

    add unit test

--- app/shared/services/contact.service.spec.js
+++ app/shared/services/contact.service.spec.js
@@ -1,8 +1,35 @@
 describe('Contact Service', function() {
+  var ContactService, $httpBackend;
 
-	it('has a dummy spec to test 2 + 2', function() {
-    // An intentionally failing test. No code within expect() will never equal 4.
-    expect(4).toEqual(4);
+  // Load our contactService module
+  beforeEach(angular.mock.module('myApplication'));
+
+  // Set our injected Contact Service (_Contact_) to our local Contact variable
+  beforeEach(
+    inject(function(_contactService_, _$injector_) {
+      ContactService = _contactService_;
+      $httpBackend = _$injector_.get('$httpBackend');
+
+      contactListHandler = $httpBackend.when('GET', 'http://localhost:3000/contacts').respond({
+        contacts: [
+          {
+            id: 1,
+            fullName: 'Alessandro Magno',
+            yearOfBirth: '1980',
+          },
+        ],
+      });
+    })
+	);
+
+
+  // A simple test to verify the Users service exists
+  it('Contact service should exist', function() {
+    expect(ContactService).toBeDefined();
   });
 
+  it('Check if list of contact is empty', function() {
+    expect(ContactService.contactList).toEqual(undefined);
+	});
+	
 });
